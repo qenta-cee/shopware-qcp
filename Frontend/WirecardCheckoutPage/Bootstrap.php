0f998cea5e8d2dfbd5f277bd1945cd71f1dee6bb
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-02-05 09:19:02 +0100

    #61 Update ratepay script loading

--- Frontend/WirecardCheckoutPage/Bootstrap.php
+++ Frontend/WirecardCheckoutPage/Bootstrap.php
@@ -956,12 +956,14 @@ class Shopware_Plugins_Frontend_WirecardCheckoutPage_Bootstrap extends Shopware_
                     $consumerDeviceId = md5(Shopware()->WirecardCheckoutPage()->getConfig()->customerid . "_" . $timestamp);
                     Shopware()->Session()->offsetSet('wcpConsumerDeviceId', $consumerDeviceId);
                 }
-                $ratepay = '<script language="JavaScript">var di = {t:"'.$consumerDeviceId.'",v:"WDWL",l:"Checkout"};</script>';
-                $ratepay .= '<script type="text/javascript" src="//d.ratepay.com/'.$consumerDeviceId.'/di.js"></script>';
-                $ratepay .= '<noscript><link rel="stylesheet" type="text/css" href="//d.ratepay.com/di.css?t='.$consumerDeviceId.'&v=WDWL&l=Checkout"></noscript>';
-                $ratepay .= '<object type="application/x-shockwave-flash" data="//d.ratepay.com/WDWL/c.swf" width="0" height="0"><param name="movie" value="//d.ratepay.com/WDWL/c.swf" /><param name="flashvars" value="t='.$consumerDeviceId.'&v=WDWL"/><param name="AllowScriptAccess" value="always"/></object>';
-                $view->wcpConsumerDeviceIdScript = $ratepay;
-
+                if ((Shopware()->WirecardCheckoutPage()->Config()->INVOICE_PROVIDER == 'ratepay' && $view->paymentTypeName == 'invoice') ||
+                    (Shopware()->WirecardCheckoutPage()->Config()->INVOICE_PROVIDER == 'ratepay' && $view->paymentTypeName == 'installment')) {
+                    $ratepay = '<script language="JavaScript">var di = {t:"' . $consumerDeviceId . '",v:"WDWL",l:"Checkout"};</script>';
+                    $ratepay .= '<script type="text/javascript" src="//d.ratepay.com/' . $consumerDeviceId . '/di.js"></script>';
+                    $ratepay .= '<noscript><link rel="stylesheet" type="text/css" href="//d.ratepay.com/di.css?t=' . $consumerDeviceId . '&v=WDWL&l=Checkout"></noscript>';
+                    $ratepay .= '<object type="application/x-shockwave-flash" data="//d.ratepay.com/WDWL/c.swf" width="0" height="0"><param name="movie" value="//d.ratepay.com/WDWL/c.swf" /><param name="flashvars" value="t=' . $consumerDeviceId . '&v=WDWL"/><param name="AllowScriptAccess" value="always"/></object>';
+                    $view->wcpConsumerDeviceIdScript = $ratepay;
+                }
                 if ( ! $this->isActivePayment($paymentName)) {
                     $controller->forward('shippingPayment');
                 }
